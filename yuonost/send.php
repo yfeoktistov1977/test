7dd9da8452a237237ea1c95125bb58229ca4d49b
yfeoktistov@yandex.ru <Vadim2011>
2020-01-18 17:37:14 +0300

    add send form, add extra pictures

---
+++ yuonost/send.php
@@ -0,0 +1,27 @@
+<?php 
+
+if(isset($_POST['submit'])){
+    $to = "yfeoktistov@yandex.ru"; // Здесь нужно написать e-mail, куда будут приходить письма
+    $from = "yfeoktistov@yndex.ru"; // Здесь нужно написать e-mail, от кого будут приходить письма, например no-reply@epicblog.net
+    $first_name = $_POST['first_name'];
+    $subject = "Форма отправки сообщений с сайта";
+    $subject2 = "Copy of your form submission";
+    $message = "ФИО посетителя: ". $first_name . " | Адрес электронной почты: "  . $_POST['email'] . " | Комментарий: " . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+	
+    mail($to,$subject,$message,$headers);
+   // mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender - Отключено!
+    echo "Сообщение отправлено. Спасибо Вам " . $first_name . ", мы скоро свяжемся с Вами.";
+	echo "<br /><br /><a href='http://younost'>Вернуться на сайт.</a>";
+
+}
+
+?>
+<!--Переадресация на главную страницу сайта, через 3 секунды-->
+<script language="JavaScript" type="text/javascript">
+function changeurl(){eval(self.location="http://younost");}
+window.setTimeout("changeurl();",3000);
+</script>
