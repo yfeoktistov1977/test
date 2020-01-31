982a44f771a48013c95694a1fde63f030d0ddc7f
yfeoktistov@yandex.ru <Vadim2011>
2020-01-31 17:44:02 +0300

    Add simple bot protection

--- yuonost/send.php
+++ yuonost/send.php
@@ -1,25 +1,27 @@
 <?php 
 
-if(isset($_POST['submit'])){
-    $to = "info@un-moscow.ru";
-    $to_copy = "innaysemenova@yandex.ru";
-    //$from = "-f yfeoktistov1977@mail.ru"; // Здесь нужно написать e-mail, от кого будут приходить письма
-    $first_name = $_POST['first_name'];
-    $subject = "Сообщение с сайта un-moscow.ru для тренера";
-    //$subject2 = "Copy of your form submission";
-    $message = "ФИО посетителя: ". $first_name . "\n Контактный телефон "  . $_POST['phone'] . "\n Сообщение для тренера: " . $_POST['message'];
+if (isset($_POST['submit']) && $_POST['btest']=='') {
+	$to = "info@un-moscow.ru";
+	$to_copy = "innaysemenova@yandex.ru";
+	//$from = "-f yfeoktistov1977@mail.ru"; // Здесь нужно написать e-mail, от кого будут приходить письма
+	$first_name = $_POST['first_name'];
+	$subject = "Сообщение с сайта un-moscow.ru для тренера";
+	//$subject2 = "Copy of your form submission";
+	$message = "ФИО посетителя: ". $first_name . "\n Контактный телефон "  . $_POST['phone'] . "\n Сообщение для тренера: " . $_POST['message'];
 
-    mail($to_copy, $subject, $message);
-    $res = mail($to, $subject, $message);
-    if($res = true) {
-        echo "Сообщение отправлено. Спасибо Вам " . $first_name . ", мы скоро свяжемся с Вами.";
-    	echo "<br /><br /><a href='http://un-moscow.ru/'>Вернуться на сайт.</a>";
-    }
+	mail($to_copy, $subject, $message);
+	$res = mail($to, $subject, $message);
+	if($res = true) {
+		echo "Сообщение отправлено. Спасибо Вам " . $first_name . ", мы скоро свяжемся с Вами.";
+		echo "<br /><br /><a href='http://un-moscow.ru/'>Вернуться на сайт.</a>";
+	}
 }
 
 ?>
 <!--Переадресация на главную страницу сайта, через 3 секунды-->
 <script language="JavaScript" type="text/javascript">
-function changeurl(){eval(self.location="http://un-moscow.ru/");}
-window.setTimeout("changeurl();",3000);
+	function changeurl() {
+		eval(self.location="http://un-moscow.ru/");
+	}
+	window.setTimeout("changeurl();", 5000);
 </script>
