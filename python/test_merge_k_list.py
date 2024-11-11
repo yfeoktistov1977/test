e0d58df77e8691c35f3638c6ab63a32e2e3ca2ca
y_feoktistov <y_feoktistov@ntc-raduga.ru>
2024-11-11 12:15:20 +0300

    merge k linked lists works

--- python/test_merge_k_list.py
+++ python/test_merge_k_list.py
@@ -8,17 +8,51 @@ class ListNode:
          self.val = val
          self.next = next
 class MergeListHelper:
-	def MergeKList(self, lists : List[List]) -> List:
-		for i in lists:
-			for j in i:
-				print(f"j item= {j}")
+	def makeListFromArray(self, lists : list[int]) -> ListNode:
+		head = ListNode(lists[0], None)
+		current = head
+		for i in lists[1:]:
+			current.next = ListNode(i, None)
+			current = current.next
+		return head
 
-	def MergeKListLinked(self, lists : list[Optional[ListNode]]) -> ListNode:
+
+	def PrintKListLinked(self, lists : List[Optional[ListNode]]):
 		for i in lists:
-			j = i
+			next_item = i
+			print("Printing next list >>")
 			while True:
-				print("item =" + str(j.val))
-				j=j.next
-				if j.next == None:
-					print("item =" + str(j.val))
+				print("val = " + str(next_item.val))
+				if next_item.next == None:
 					break;
+				next_item = next_item.next
+
+	def mergeTwoLists(self, l1 : ListNode, l2 : ListNode) -> ListNode:
+		dummy = ListNode(0, None)
+		current = dummy
+
+		while l1 and l2:
+			if l1.val < l2.val:
+				current.next = l1
+				l1 = l1.next
+			else:
+				current.next = l2
+				l2 = l2.next
+			current = current.next
+ 
+    		# Append the remaining nodes of either list
+		current.next = l1 if l1 else l2
+		return dummy.next
+
+	def merge_k_lists(self, lists: List[ListNode]) -> ListNode:
+		if not lists:
+			return None
+ 
+		merged_list = None
+ 
+		for i in range(len(lists)):
+			merged_list = self.mergeTwoLists(merged_list, lists[i])
+ 
+		return merged_list
+
+
--- python/test_py.py
+++ python/test_py.py
@@ -6,8 +6,9 @@ import test_merge_k_list
 import test_func
 import os
 import sys
-from test_merge_k_list import MergeListHelper
-from test_merge_k_list import ListNode
+#from test_merge_k_list import MergeListHelper
+#from test_merge_k_list import ListNode
+from test_merge_k_list import *
 
 gvalue = 125
 
@@ -26,47 +27,49 @@ def ask_yes_no(question = "Do you want ? (y/n)"):
         return response
 
 #program -------------------------
+if __name__ == "__main__":
+	data_array = [8, 3, 10, 14, 1]
+	data_array1 = list(data_array)
+	data_array1[0] = 155
 
-data_array = [8, 3, 10, 14, 1]
-data_array1 = list(data_array)
-data_array1[0] = 155
-
-data_dict = { 1 : 1, 2 : 333}
+	data_dict = { 1 : 1, 2 : 333}
 
 
-#data_array1 = [[4,5,6], [1,3], [4,5] , [1,1], [1,3]]
+	#data_array1 = [[4,5,6], [1,3], [4,5] , [1,1], [1,3]]
 
-#test_py_clib.use_clib()
-#display("234234234");
-#print("Global variable(place2) = ",  gvalue)
-#test_windows.show_window_class()
+	#test_py_clib.use_clib()
+	#display("234234234");
+	#print("Global variable(place2) = ",  gvalue)
+	#test_windows.show_window_class()
 
-#ask_yes_no()
+	#ask_yes_no()
 
-#if test_py_if_for.test_if() !=0:
-#       sys.exit(0)
-#else:
-#	print("Access granted !");
+	#if test_py_if_for.test_if() !=0:
+	#       sys.exit(0)
+	#else:
+	#	print("Access granted !");
 
-#test_py_if_for.test_loops()
+	#test_py_if_for.test_loops()
 
-#test_func.array_buble_sort(data_array)
-#print(data_array)
+	#test_func.array_buble_sort(data_array)
+	#print(data_array)
 
-#test_func.param_test_func("888")
+	#test_func.param_test_func("888")
 
-test_list = [[1,3], [5,7], [2,6]]
-testMergeClass = MergeListHelper()
-testMergeClass.MergeKList(test_list)
+	#------- start>> merge k linked lists sorted by accend
+	test_list = [[1,3], [5,7], [2,6]]
+	testMergeClass = MergeListHelper()
+	Main_list = [testMergeClass.makeListFromArray(test_list[0]),
+              	testMergeClass.makeListFromArray(test_list[1]),
+                testMergeClass.makeListFromArray(test_list[2])]
+	print(f"(2)------------------")
+	Merged_List = testMergeClass.merge_k_lists(Main_list)
+	next_item = Merged_List
+	while True:
+		print(f"Next.item {next_item.val}");
+		next_item = next_item.next
+		if not next_item.next:
+			break
+	#------- end merge k linked lists sorted by accend
 
-A2 = ListNode(3, None)
-A1 = ListNode(1, A2)
-B2 = ListNode(7, None)
-B1 = ListNode(5, B2)
-C2 = ListNode(6, None)
-C1 = ListNode(2, C2)
-
-Main_list = [A1, B1, C1]
-testMergeClass.MergeKListLinked(Main_list)
-
-print(f" End of program !")
+	print(f" End of program !")
